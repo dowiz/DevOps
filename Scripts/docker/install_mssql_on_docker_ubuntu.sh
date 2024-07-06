41a03c23d27aeac436b5be8b78539e78feb9df36
Олександр Єрьомін <eriomin.oe@gmail.com>
2024-07-06 11:10:44 +0300

    added a recreate folder for volumes

--- Scripts/docker/install_mssql_on_docker_ubuntu.sh
+++ Scripts/docker/install_mssql_on_docker_ubuntu.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 sudo rm -rf /opt/mssql/
-sudo chmod 777 /opt/mssql/
+sudo mkdir /opt/mssql/
+sudo chmod 777 -R /opt/mssql/
 sudo docker run -v /opt/mssql:/var/opt/mssql/ -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Qwerty-1" -p 1433:1433 --name docker_MSSQL --hostname docker_MSSQL -d mcr.microsoft.com/mssql/server:2022-latest
 sudo docker ps
